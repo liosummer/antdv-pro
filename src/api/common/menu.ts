66582b7c564845f95b8f05e3b643ff14038f5fd6
aibayanyu20 <aibayanyu@qq.com>
2023-07-14 09:48:10 +0800

    fix: change url

--- src/api/common/menu.ts
+++ src/api/common/menu.ts
@@ -2,12 +2,12 @@ import { isArray } from '@v-c/utils'
 import type { MenuData } from '~@/layouts/basic-layout/typing'
 import { camelCaseObj } from '~@/utils/utils'
 
-export const getRouteMenusApi = async() => {
-  const res = await useGet<MenuData>('/menu')
-  if(isArray( res.data)&& res.data.length>0){
-    res.data = res.data.map(v=>{
-      return camelCaseObj(v);
+export const getRouteMenusApi = async () => {
+  const res = await useGet<MenuData>('/user/menu')
+  if (isArray(res.data) && res.data.length > 0) {
+    res.data = res.data.map((v) => {
+      return camelCaseObj(v)
     })
   }
-  return res;
+  return res
 }
